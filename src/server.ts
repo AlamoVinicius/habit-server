146e958c12c58f22a60fdc75ee79b7cec51599f1
AlamoVinicius <alamo.cma22@gmail.com>
2023-01-18 01:07:33 -0300

    feat: create habit add

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,17 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 const PORT = 3333;
 
 app.register(cors);
-
-app.get("/hello", async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: "Beber",
-      },
-    },
-  });
-  //   console.log(habits);
-  return habits;
-});
+app.register(appRoutes);
 
 app
   .listen({
     port: PORT,
   })
-  .then(() => console.log(`server is run at port ${PORT}...`));
+  .then(() =>
+    console.log(`server is run at link http://localhost:${PORT}/...`)
+  );
