56093271a027f1ff121503c7360270f80cec4798
AlamoVinicius <alamo.cma22@gmail.com>
2023-01-18 20:54:57 -0300

    feat: get habits of the day implemented

--- src/routes.ts
+++ src/routes.ts
@@ -28,6 +28,52 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
   });
+
+  app.get("/day", async (request) => {
+    try {
+      const getDayParams = z.object({
+        date: z.coerce.date(),
+      });
+
+      const { date } = getDayParams.parse(request.query);
+
+      const parsedDay = dayjs(date).startOf("day");
+      const weekDay = dayjs(date).get("day");
+
+      const possibleHabits = await prisma.habit.findMany({
+        where: {
+          created_at: {
+            lte: date,
+          },
+          weekDays: {
+            some: {
+              week_day: weekDay,
+            },
+          },
+        },
+      });
+
+      const day = await prisma.day.findUnique({
+        where: {
+          date: parsedDay.toDate(),
+        },
+        include: {
+          dayHabits: true,
+        },
+      });
+
+      const completedHabits = day?.dayHabits.map(
+        (dayHabits) => dayHabits.habit_id
+      );
+
+      return {
+        possibleHabits,
+        completedHabits,
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  });
 }
 
 // advantages zod: typeScript automatically and validation
